0d826236b240d80a8b974de3a06afcebb16927cf
dlwangyilin <Linyiwang_3218>
2020-07-26 22:14:40 -0700

    Add upload pictures options and validators for file types. Change form to Reactive form. NOT template driven form.

--- frontend/src/app/posts/post-create/post-create.component.ts
+++ frontend/src/app/posts/post-create/post-create.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForm} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {PostsService} from '../posts.service';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 import {Post} from '../post.model';
+import {mimeType} from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -15,26 +16,25 @@ export class PostCreateComponent implements OnInit{
   private postId: string;
   public post: Post;
   public isLoading = false;
+  public form: FormGroup;
+  public imagePreview;
 
   constructor(public postsService: PostsService,
               public route: ActivatedRoute) {
   }
 
-  onSavePost(form: NgForm): void {
-    if (form.invalid) {
-      return;
-    }
-    this.isLoading = true;
-
-    if (this.mode === 'create') {
-      this.postsService.addPosts(form.value.title, form.value.content);
-    } else {
-      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
-    }
-    form.resetForm();
-  }
-
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]}),
+      content: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      image: new FormControl(null, {
+        validators: Validators.required,
+        asyncValidators: mimeType
+      })
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
         this.mode = 'edit';
@@ -47,10 +47,38 @@ export class PostCreateComponent implements OnInit{
             title: postData.title,
             content: postData.content};
         });
+        this.form.setValue({
+          title: this.post.title,
+          content: this.post.content
+        });
       } else {
         this.mode = 'create';
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event): void {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onSavePost(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    if (this.mode === 'create') {
+      this.postsService.addPosts(this.form.value.title, this.form.value.content);
+    } else {
+      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
+    }
+    this.form.reset();
   }
 }
