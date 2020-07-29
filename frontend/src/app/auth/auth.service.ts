8283ae2383714f84395a22b9e284f7dba523743e
dlwangyilin <Linyiwang_3218>
2020-07-29 00:27:25 -0700

    Add redirection after login and signup

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,12 +1,18 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AuthData} from './auth-data';
+import {Subject} from 'rxjs';
+import {falseIfMissing} from 'protractor/built/util';
+import {Router} from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 
+  private isAuthenticated = false;
   private token: string;
-  constructor(private http: HttpClient) {
+  private authStatusListener = new Subject<boolean>();
+
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   createUser(email: string, password: string) {
@@ -28,11 +34,30 @@ export class AuthService {
     this.http.post<{token: string}>('http://localhost:4201/api/user/login', authData)
       .subscribe(response => {
         this.token = response.token;
-      })
+        if (this.token) {
+          this.authStatusListener.next(true);  // notify listeners that token is valid
+          this.isAuthenticated = true;
+          this.router.navigate(['/']);
+        }
+      });
   }
 
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+    this.router.navigate(['/']);
+  }
+
+  getIsAuth() {
+    return this.isAuthenticated;
+  }
 
   getToken(): string {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 }
