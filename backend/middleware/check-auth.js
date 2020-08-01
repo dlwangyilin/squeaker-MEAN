e54002326331f7c049de6895e9abd6b37a44c45c
dlwangyilin <Linyiwang_3218>
2020-07-31 23:49:09 -0700

    Extract user data in backend and only update and delete post if publisher is the current user.

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -5,14 +5,12 @@ module.exports = (req, res, next) => {
     // authorization: "Bearer
     try {
         const token = req.headers.authorization.split(' ')[1];  // extract token
-        jwt.verify(token, process.env.PRIVATE_SECRET_KEY);
+        const decodedToken = jwt.verify(token, process.env.PRIVATE_SECRET_KEY);
+        req.userData = { email: decodedToken.email, userId: decodedToken.userId};  // As we add them when generating tokens.
         next();
     } catch (e) {
         res.status(401).json({
             message: "Auth failed!"
         });
     }
-
-
-
 };
